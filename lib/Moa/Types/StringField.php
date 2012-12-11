2668f4f5740e79f970ac397eef32944e484cb732
Luke Cawood <luke.cawood@99designs.com>
2012-12-12 01:38:32 +1100

    Change validation and type coercion rules for StringFields

--- lib/Moa/Types/StringField.php
+++ lib/Moa/Types/StringField.php
@@ -7,7 +7,23 @@ class StringField extends Type
     public function validate($value)
     {
         parent::validate($value);
-        if (isset($value) && !is_string($value))
-            $this->error('is not a string');
+        if (isset($value) && !$this->isStringable($value))
+            $this->error('is not stringable');
     }
+
+    public function toMongo(&$doc, &$mongoDoc, $key)
+    {
+        if (isset($doc[$key]))
+            $mongoDoc[$key] = (string)$doc[$key];
+    }
+
+	/**
+	 * Scalar values and any object with a __toString method can be cast to a string
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	private function isStringable($value)
+	{
+		return (is_scalar($value) || (is_object($var) && is_callable(array($value, '__toString'))));
+	}
 }
