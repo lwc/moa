00e4ae2579f59ec191e21e8f53dbae3ca48e9bc0
Luke Cawood <luke.cawood@99designs.com>
2012-12-12 01:37:32 +1100

    Change validation and type coercion rules for BooleanFields

--- lib/Moa/Types/BooleanField.php
+++ lib/Moa/Types/BooleanField.php
@@ -7,7 +7,13 @@ class BooleanField extends Type
     public function validate($value)
     {
         parent::validate($value);
-        if (isset($value) && !is_bool($value))
-            $this->error('is not boolean');
+        if (isset($value) && !is_scalar($value))
+            $this->error('is not scalar');
+    }
+
+    public function toMongo(&$doc, &$mongoDoc, $key)
+    {
+        if (isset($doc[$key]))
+            $mongoDoc[$key] = (bool)$doc[$key];
     }
 }
